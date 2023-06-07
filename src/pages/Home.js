f4c2ffe1f89deb504ffbcbb77ec62b43f6fe030f
mithu <mithu42329@gmail.com>
2023-06-08 02:17:24 +0530

    tried more banner images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
-import BannerImage from "../images/banner1.jpg";
+import BannerImage from "../images/banner8.jpg";
 import "../styles/HomeStyle.css";
 
 const Home = () => {
     return (
         <Layout>
             <div className="home" style={{
-                backgroundImage: `url(${BannerImage})`, backgroundSize: "cover", backgroundPosition:"left center"}}>
+                backgroundImage: `url(${BannerImage})`, backgroundSize:"cover", backgroundPosition:"left bottom"}}>
                 <div className="homeHeader">
                     <h1>
                         Fresh Vegetables Shop
